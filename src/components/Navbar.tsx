20b9b37022ae1b234ab21fd6e40b44fa698044df
madiiikkk <madik2212@gmail.com>
2025-02-10 07:17:48 +0500

    что то новое

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../styles/Navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import LoginModal from './LoginModal';
+import '../styles/Navbar.css';
 
 function Navbar() {
-  return (
-    <div>
-        <nav className='navbar'>
-            <div className='links'>
-                <Link to="/" className="logo">
-                    PolyGlot
-                </Link>
-            </div>
-            <div className='links'>
-                <Link to="/Account" className="link">
-                    Account
-                </Link>
-            </div>
-            <div className='links'>
-                <Link to="/QuizPage" className="link">
-                    QuizPage
-                </Link>
-            </div>
-            <div className='links'>
-                <Link to="/Roadmap" className="link">
-                    Roadmap
-                </Link>
-            </div>
-            <div className='links'>
-                <button className="Authorizathion_off">
-                    SignIn
-                </button>
-            </div>
-            
-            <a className="links"></a>
-        </nav>
+    const [isVisible, setVisible] = useState(false);
 
-    </div>
-  )
+    return (
+        <div>
+            <nav className='navbar'>
+                <div className='logoLink'>
+                    <Link to="/" className="logo">
+                        PolyGlot
+                    </Link>
+                </div>
+                <div className='links'>
+                    <Link to="/Account" className="link">
+                        Account
+                    </Link>
+                </div>
+                <div className='links'>
+                    <Link to="/QuizPage" className="link">
+                        QuizPage
+                    </Link>
+                </div>
+                <div className='links'>
+                    <Link to="/Roadmap" className="link">
+                        Roadmap
+                    </Link>
+                </div>
+                <div className='links'>
+                    <button className="Login" onClick={() => setVisible(!isVisible)}>
+                        SignIn
+                    </button>
+                </div>
+            </nav>
+
+            <LoginModal isVisible={isVisible} setVisible={setVisible} />
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../styles/HomePage.css'
 import { Link } from 'react-router-dom'
+import firstimage from "../assets/reading.png"
+import okno from "../assets/okno.png"
+import meditateQyz from "../assets/meditating.png"
 import Navbar from '../components/Navbar'
 
 
@@ -9,8 +12,30 @@ export default function HomePage() {
     <div>
       <Navbar/>
       <div className="first_block">
-        
+        <img src={firstimage} alt="firstimage" className="first_image" />
+        <b className="name">PolyGlot - learn a lot!</b>
+        <b className="description">It is a platform for learning three languages Kazakh, English and Russian. 
+          There will be a quiz and a personal account with progress. </b>
+      </div>
+      <div className="second_block">
+        <div className="info">
+          <b className="info_top">
+            Welcome to Polyglot
+            </b>
+          <b className="info_name">
+            Your Personal Language Tutor
+          </b>
+          <b className="info_descrip">
+            Learn languages in a fun and interactive way. 
+            Practice daily and track your progress!
+          </b>
         </div>
+        <div className="containerW_img">
+          <img src={okno} alt="okno1" className="container" />
+          <img src={okno} alt="okno2" className="containerdva" />
+          <img src={meditateQyz} alt="chel" className="cheldaun" />
+        </div>
+      </div>
     </div>
   )
 }
